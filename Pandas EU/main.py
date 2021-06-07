802ca9288e71dfa8ecfb131d440b3cc1cae0b66e
Nicolae <67197181+Nicolae77@users.noreply.github.com>
2021-06-07 13:00:16 +0100

    Update main.py

--- Pandas EU/main.py
+++ Pandas EU/main.py
@@ -33,4 +33,25 @@ while len(guessed_country) < 39:
         country_data = data[data.country == answer_country]
         t.goto(int(country_data.x), int(country_data.y))
         t.write(answer_country)
+        answer_cities = screen.textinput(title=f"{len(guessed_cities)}/39 Guess the Capital",
+                                     prompt="What's the capital of this Country?").title()
+    if answer_cities == 'Exit':
+        missing_cities = []
+        for city in all_cities:
+            if city not in guessed_cities:
+                missing_cities.append(city)
+        city_data = pandas.DataFrame(missing_cities)
+        city_data.to_csv("cities_to_learn")
+        learn_cities = screen.textinput(title="You should learn next cities", prompt=f'{city_data}')
+        break
+
+    if answer_cities in all_cities:
+        guessed_cities.append(answer_cities)
+        ts = turtle.Turtle()
+        ts.hideturtle()
+        ts.penup()
+        cities_data = data[data.cities == answer_cities]
+        ts.goto(int(cities_data.z), int(cities_data.a))
+        ts.write(answer_cities)    
+    
 
